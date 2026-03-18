5069cd0c92a8b0d3e619442c9e1dc58f836381cb
1002manthan <manthansuthar789@gmail.com>
2026-03-18 17:50:11 +0530

    Added Cursor Pointer feature and updated about me section

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3825294217" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1661315039" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
